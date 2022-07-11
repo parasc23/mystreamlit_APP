c270fc52cd81a165820a30bf8c276e15938dcc45
parasc23 <108923407+parasc23@users.noreply.github.com>
2022-07-12 01:40:05 +0530

    update code

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -3,13 +3,13 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-path = r"C:\Users\paras.chauhan\OneDrive - Mars Inc\Desktop\Agrocel\Stripping Plant\New_Model(RPM)\Streamlit_Data.xlsx"
+path = r"Streamlit_Data.xlsx"
 
-st.set_page_config(page_title="Correlation Chart", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Correlation Chart", page_icon=":bar_chart:", layout="long")
 
+st.title("**Correlation Chart of S5 and S7 with Efficiency**", anchor=None)
 
 Plant = st.radio("PLANT",('S5','S7'))
-@st.cache
 def get_data_from_excel(sheet):
     df = pd.read_excel(path,sheet_name=sheet,parse_dates=['Date'],na_filter=True)
     return df
@@ -31,7 +31,6 @@ correlation_data = df_selection.corr()['Efficiency'][:-1]
 fig_corr_chart = px.bar(
     x = correlation_data,
     y=correlation_data.index,
-    title="<b>Correlation Between Efficency and features of {}".format(Plant),
     template="plotly_white",width=1200,height=800)
 
 st.plotly_chart(fig_corr_chart)
